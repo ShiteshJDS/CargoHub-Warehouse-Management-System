83bdd4d626e88b532615905fc879edd1e4e2519f
Aidan90009 <aidantsoi06@gmail.com>
2024-10-23 11:28:23 +0200

    Final Warhouse Tests

--- api/Tests/Warehouse_test.py
+++ api/Tests/Warehouse_test.py
@@ -10,11 +10,13 @@ import pytest
 import requests
 import logging
 
+
 # Add the path to the CargoHub directory to sys.path
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.insert(0, os.path.abspath(
 os.path.join(os.path.dirname(__file__), '..')))
 from models.warehouses import Warehouses
+from main import StartWebAPI
 
 
 BASE_URL = "http://localhost:3000"  # Replace with your API's base URL
@@ -25,7 +27,13 @@ class Test_Warehouses():
 
     warehousesObject = Warehouses("Test_Data/test_")
 
+
+    ########## Test Endpoint ##########
+
+    # server must be running when testing endpoints
+    
     def test_correct_get_endpoint(self):
+        # StartWebAPI()
         headers = {
             "API_KEY": "a1b2c3d4e5",
             "Content-Type": "application/json"
@@ -96,6 +104,8 @@ class Test_Warehouses():
             f"{BASE_URL}/api/v1/warehouses/{newWarehouseJson['id']}", headers=deleteHeader)
         assert responseDelete.status_code == 200
 
+
+    ########## Test Warehouses Methods ##########
     def test_get_warehouses(self):
 
         warehousesInJson = self.warehousesObject.get_warehouses()
@@ -134,7 +144,7 @@ class Test_Warehouses():
                 "created_at": "2008-02-22 19:55:39",
                 "updated_at": "2009-08-28 23:15:50"
             }
-        ]
+        ], "The warehouse with ID 2 doesn't match the expected dictionary"
 
     def test_get_warehouse_with_id(self):
         warehouseWithId2 = self.warehousesObject.get_warehouse(2)
@@ -154,7 +164,7 @@ class Test_Warehouses():
             },
             "created_at": "2008-02-22 19:55:39",
             "updated_at": "2009-08-28 23:15:50"
-        }
+        }, "The warehouse with id 2 doesn't match the dictionary"
 
     def test_add_warehouse(self):
         newWarehouse = {
@@ -182,6 +192,42 @@ class Test_Warehouses():
                warehouseFromDB["zip"] == newWarehouse["zip"] and\
                warehouseFromDB["province"] == newWarehouse["province"] and \
                warehouseFromDB["country"] == newWarehouse["country"] and \
-               warehouseFromDB["contact"] == newWarehouse["contact"]
+               warehouseFromDB["contact"] == newWarehouse["contact"], \
+               "The json doesn't match the created newWarehouse dictionary , or get_warehouse doesn't function properly"
+    def test_update_warehouse(self):
+        
+
+        newUpdatedWarehouse = {
+            "id": 3, 
+            "code": "ABCDEFG", 
+            "name": "Rotterdam distribution hub", 
+            "address": "Izesteeg 807", 
+            "zip": "1636 KI", "city": "Rotterdam", 
+            "province": "Zuid-Holland", 
+            "country": "NL", 
+            "contact": 
+                {"name": "Fred van Wallaert", 
+                 "phone": "(009) 123456789", 
+                 "email": "jelle77@example.net"
+                }, 
+            "created_at": "2001-05-11 10:43:52", 
+            "updated_at": "2017-12-19 14:32:38"
+        }
+
+        self.warehousesObject.update_warehouse(3, newUpdatedWarehouse)
+        warehouseFromDB = self.warehousesObject.get_warehouse(3)
+        assert warehouseFromDB["id"] == newUpdatedWarehouse["id"] and \
+               warehouseFromDB["code"] == newUpdatedWarehouse["code"] and\
+               warehouseFromDB["name"] ==  newUpdatedWarehouse["name"] and\
+               warehouseFromDB["address"] == newUpdatedWarehouse["address"] and\
+               warehouseFromDB["zip"] == newUpdatedWarehouse["zip"] and\
+               warehouseFromDB["province"] == newUpdatedWarehouse["province"] and \
+               warehouseFromDB["country"] == newUpdatedWarehouse["country"] and \
+               warehouseFromDB["contact"] == newUpdatedWarehouse["contact"] ,\
+               "The JSON response doesn't match the updated newUpdatedWarehouse dictionary, or get_warehouse doesn't function properly."
+
+    def test_remove_warehouse(self):
 
         self.warehousesObject.remove_warehouse(3)
+        assert self.warehousesObject.get_warehouse(3) == None, \
+        "Warehouse with ID 3 still exists in the database, or get_warehouse doesn't function properly."
