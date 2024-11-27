595a1fde75b6df606b150534b68c613fa1c87107
ShiteshJDS <1083242@hr.nl>
2024-11-27 10:37:59 +0100

    Meer comments voor readability

--- api/main.py
+++ api/main.py
@@ -59,6 +59,7 @@ class ApiRequestHandler(http.server.BaseHTTPRequestHandler):
             "item_types": data_provider.fetch_item_type_pool(),
         }
 
+        # kijkt of de recource bestaat in de dictionary
         if path[0] not in pools:
             self.send_response(404)
             self.end_headers()
@@ -68,17 +69,22 @@ class ApiRequestHandler(http.server.BaseHTTPRequestHandler):
         paths = len(path)
 
         try:
+            # Verwerk verzoeken met alleen de resource-naam (bijv. "/items").
             if paths == 1:
                 if hasattr(pool, "get_" + path[0]):
                     send_json_response(getattr(pool, "get_" + path[0])())
                 else:
                     send_json_response(None, 404)
+
+            # Verwerk verzoeken met een resource en een identificator (bijv. "/items/123").
             elif paths == 2:
                 identifier = int(path[1]) if path[0] != "items" else path[1]
                 if hasattr(pool, "get_" + path[0][:-1]):
                     send_json_response(getattr(pool, "get_" + path[0][:-1])(identifier))
                 else:
                     send_json_response(None, 404)
+
+            # Verwerk verzoeken met een resource, een identificator en een geneste resource (bijv. "/warehouses/123/items").
             elif paths == 3:
                 if path[2] in ["locations", "items", "inventory", "orders"]:
                     if hasattr(pool, "get_" + path[2] + "_in_" + path[0][:-1]):
@@ -95,6 +101,8 @@ class ApiRequestHandler(http.server.BaseHTTPRequestHandler):
                         send_json_response(None, 404)
                 else:
                     send_json_response(None, 404)
+
+            # Verwerk een speciale case voor inventory-totals (bijv. "/items/123/inventory/totals").
             elif paths == 4 and path[2] == "inventory" and path[3] == "totals":
                 if hasattr(pool, "get_inventory_totals_for_item"):
                     send_json_response(pool.get_inventory_totals_for_item(path[1]))
