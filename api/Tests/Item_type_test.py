a585a04a6819e03a3f8639e73a914f67def3b4b2
JAYDEN1348 <1071427@hr.nl>
2024-11-27 09:17:56 +0100

    item type testing completed

--- api/Tests/Item_type_test.py
+++ api/Tests/Item_type_test.py
@@ -5,6 +5,7 @@ import sys
 import os
 import requests
 import logging
+import copy
 
 
 # Add the path to the CargoHub directory to sys.path
@@ -37,42 +38,179 @@ class Test_ItemTypes():
 
     # ItemType Endpoint Testing (server must be running when testing endpoints)
 
-    def test_post_endpoint(self):
+    def test_post_endpoints(self):
 
-        responsePost = requests.post(
-            f"{BASE_URL}/api/v1/item_types/", headers=self.headers_full, json=self.newItemType)
-        new_timestamp = self.item_typeObject.get_timestamp()
-        self.newItemType["created_at"] = new_timestamp.split('T')[0]
-        self.newItemType["updated_at"] = new_timestamp.split('T')[0]
-        assert responsePost.status_code == 201
+        def test_post_correct_endpoint():
+            responsePost = requests.post(
+                f"{BASE_URL}/api/v1/item_types/", headers=self.headers_full, json=self.newItemType)
+            new_timestamp = self.item_typeObject.get_timestamp()
+            self.newItemType["created_at"] = new_timestamp.split('T')[0]
+            self.newItemType["updated_at"] = new_timestamp.split('T')[0]
+            assert responsePost.status_code == 201, "test_post_correct_endpoint"
 
-    def test_update_endpoint(self):
+        def test_post_existing_id_endpoint():
+            responsePost = requests.post(
+                f"{BASE_URL}/api/v1/item_types", headers=self.headers_full, json=self.newItemType)
+            assert responsePost.status_code == 403, "test_post_existing_id_endpoint"
 
-        self.newItemType["name"] = "Electronics"
-        self.newItemType["description"] = "Things with wires"
+        def test_post_missing_items_endpoint():
+            missing_items_item_group = copy.deepcopy(self.newItemType)
+            for i in ['id', 'name', 'description']:
+                missing_items_item_group.pop(i)
 
-        responsePut = requests.put(
-            f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=self.headers_full, json=self.newItemType)
-        self.newItemType["updated_at"] = self.item_typeObject.get_timestamp().split('T')[
-            0]
-        assert responsePut.status_code == 200
+            responsePost = requests.post(
+                f"{BASE_URL}/api/v1/item_types", headers=self.headers_full, json=missing_items_item_group)
+            assert responsePost.status_code == 403, "test_post_missing_items_endpoint"
 
-    def test_get_endpoint(self):
+        def test_post_extra_items_endpoint():
+            extra_items_item_group = copy.deepcopy(self.newItemType)
+            extra_items_item_group.update({"a": 1, "b": 2, "c": 3})
 
-        responseGet = requests.get(
-            f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=self.headers_full)
-        assert responseGet.status_code == 200
+            responsePost = requests.post(
+                f"{BASE_URL}/api/v1/item_types", headers=self.headers_full, json=extra_items_item_group)
+            assert responsePost.status_code == 403, "test_post_extra_items_endpoint"
 
-        dict_response = responseGet.json()
-        dict_response["created_at"] = dict_response["created_at"].split('T')[0]
-        dict_response["updated_at"] = dict_response["updated_at"].split('T')[0]
-        assert dict_response == self.newItemType
+        def test_post_wrong_types_endpoint():
+            wrong_types_item_group = copy.deepcopy(self.newItemType)
+            wrong_types_item_group.update(
+                {"id": True, "name": 1, "description": [1, 2, 3]})
 
-    def test_delete_endpoint(self):
+            responsePost = requests.post(
+                f"{BASE_URL}/api/v1/item_types", headers=self.headers_full, json=wrong_types_item_group)
+            assert responsePost.status_code == 403, "test_post_wrong_types_endpoint"
 
-        responseDelete = requests.delete(
-            f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=self.headers_full)
-        assert responseDelete.status_code == 200
+        def test_post_empty_values_endpoint():
+            empty_values_item_group = copy.deepcopy(self.newItemType)
+            empty_values_item_group.update(
+                {"name": "", "description": ""})
+
+            responsePost = requests.post(
+                f"{BASE_URL}/api/v1/item_types", headers=self.headers_full, json=empty_values_item_group)
+            assert responsePost.status_code == 403, "test_post_empty_values_endpoint"
+
+        test_post_correct_endpoint()
+        test_post_existing_id_endpoint()    # ?? Multiple id's
+        test_post_missing_items_endpoint()  # ?? Missing items
+        test_post_extra_items_endpoint()    # ?? Extra items
+        test_post_wrong_types_endpoint()    # ?? Wrong item types
+        test_post_empty_values_endpoint()   # ?? Empty items
+
+    def test_put_endpoints(self):
+
+        def test_put_correct_endpoint():
+            self.newItemType["name"] = "Whiteboard"
+            self.newItemType["description"] = "White Board"
+
+            responsePut = requests.put(
+                f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=self.headers_full, json=self.newItemType)
+            self.newItemType["updated_at"] = self.item_typeObject.get_timestamp().split('T')[
+                0]
+            assert responsePut.status_code == 200
+
+        def test_put_nonexistent_id_endpoint():
+            responsePut = requests.put(
+                f"{BASE_URL}/api/v1/item_types/-1", headers=self.headers_full, json=self.newItemType)
+            assert responsePut.status_code == 403, "test_put_nonexistent_id_endpoint"
+
+        def test_put_missing_items_endpoint():
+            missing_items_item_group = copy.deepcopy(self.newItemType)
+            for i in ['id', 'name', 'description']:
+                missing_items_item_group.pop(i)
+
+            responsePut = requests.put(
+                f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=self.headers_full, json=missing_items_item_group)
+            assert responsePut.status_code == 403, "test_put_missing_items_endpoint"
+
+        def test_put_extra_items_endpoint():
+            extra_items_item_group = copy.deepcopy(self.newItemType)
+            extra_items_item_group.update({"a": 1, "b": 2, "c": 3})
+
+            responsePut = requests.put(
+                f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=self.headers_full, json=extra_items_item_group)
+            assert responsePut.status_code == 403, "test_put_extra_items_endpoint"
+
+        def test_put_wrong_types_endpoint():
+            wrong_types_item_group = copy.deepcopy(self.newItemType)
+            wrong_types_item_group.update(
+                {"id": True, "name": 1, "description": [1, 2, 3]})
+
+            responsePut = requests.put(
+                f"{BASE_URL}/api/v1/item_group/{self.newItemType['id']}", headers=self.headers_full, json=wrong_types_item_group)
+            assert responsePut.status_code == 403, "test_put_wrong_types_endpoint"
+
+        def test_put_empty_values_endpoint():
+            empty_values_item_group = copy.deepcopy(self.newItemType)
+            empty_values_item_group.update(
+                {"name": "", "description": ""})
+
+            responsePut = requests.put(
+                f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=self.headers_full, json=empty_values_item_group)
+            assert responsePut.status_code == 403, "test_put_empty_values_endpoint"
+
+        test_put_correct_endpoint()
+        test_put_nonexistent_id_endpoint()  # ?? Existing id's
+        test_put_missing_items_endpoint()   # ?? Missing items
+        test_put_extra_items_endpoint()     # ?? Extra items
+        test_put_wrong_types_endpoint()     # ?? Wrong item types
+        test_put_empty_values_endpoint()    # ?? Empty items
+
+    def test_get_endpoints(self):
+
+        def test_get_by_id_correct_endpoint():
+            responseGet = requests.get(
+                f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=self.headers_full)
+            dict_response = responseGet.json()
+            dict_response["created_at"] = dict_response["created_at"].split('T')[
+                0]
+            dict_response["updated_at"] = dict_response["updated_at"].split('T')[
+                0]
+            assert responseGet.status_code == 200, "test_get_by_id_correct_endpoint"
+            assert dict_response == self.newItemType, "test_get_by_id_correct_endpoint"
+
+        def test_get_all_correct_endpoint():
+            responseGet = requests.get(
+                f"{BASE_URL}/api/v1/item_types", headers=self.headers_full)
+            assert responseGet.status_code == 200, "test_get_all_correct_endpoint"
+            assert self.newItemType["id"] in [ig["id"]
+                                              for ig in responseGet.json()], "test_get_all_correct_endpoint"
+
+        def test_get_items_correct_endpoint():
+            responseGet = requests.get(
+                f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}/items", headers=self.headers_full)
+            assert responseGet.status_code == 200, "test_get_items_correct_endpoint"
+            assert len(responseGet.json()
+                       ) == 0, "test_get_items_correct_endpoint"
+
+        def test_get_by_id_nonexistent_id_endpoint():
+            responseGet = requests.get(
+                f"{BASE_URL}/api/v1/item_types/-1", headers=self.headers_full)
+            assert responseGet.status_code == 403, "test_get_by_id_nonexistent_id_endpoint"
+
+        def test_get_locations_nonexistent_id_endpoint():
+            responseGet = requests.get(
+                f"{BASE_URL}/api/v1/item_types/-1/locations", headers=self.headers_full)
+            assert responseGet.status_code == 403, "test_get_locations_nonexistent_id_endpoint"
+
+        test_get_by_id_correct_endpoint()
+        test_get_all_correct_endpoint()
+        test_get_items_correct_endpoint()
+        test_get_by_id_nonexistent_id_endpoint()        # ?? Nonexistent id's
+        test_get_locations_nonexistent_id_endpoint()    # ?? Nonexistent id's
+
+    def test_delete_endpoints(self):
+
+        def test_delete_correct_endpoint():
+            responseDelete = requests.delete(
+                f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=self.headers_full)
+            assert responseDelete.status_code == 200, "test_delete_correct_endpoint"
+
+        def test_delete_nonexistent_id_endpoint():
+            responseDelete = requests.delete(
+                f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=self.headers_full)
+            assert responseDelete.status_code == 403, "test_delete_nonexistent_id_endpoint"
+
+        test_delete_correct_endpoint()
+        test_delete_nonexistent_id_endpoint()   # ?? Nonexistent id's
 
     def test_endpoint_restrictions(self):
 
@@ -87,15 +225,24 @@ class Test_ItemTypes():
             f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=headers_restricted, json=self.newItemType)
         responseDelete_restricted = requests.delete(
             f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=headers_restricted)
+        responseGetAll_restricted = requests.get(
+            f"{BASE_URL}/api/v1/item_types", headers=headers_restricted)
         responseGet_restricted = requests.get(
             f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}", headers=headers_restricted)
+        responseGetItems_restricted = requests.get(
+            f"{BASE_URL}/api/v1/item_types/{self.newItemType['id']}/items", headers=headers_restricted)
 
-        assert responsePost_restricted.status_code == 403
-        assert responsePut_restricted.status_code == 403
-        assert responseDelete_restricted.status_code == 403
-        assert responseGet_restricted.status_code == 200
+        assert responsePost_restricted.status_code == 403, "Post failed"
+        assert responsePut_restricted.status_code == 403, "Put failed"
+        assert responseDelete_restricted.status_code == 403, "Delete failed"
+        assert responseGetAll_restricted.status_code == 200, "Get All failed"
+        assert responseGet_restricted.status_code == 200, "Get by id failed"
+        assert responseGetItems_restricted.status_code == 200, "Get items failed"
 
-    # ItemType Method Testing
+
+class Test_Item_Types_Functions():
+
+    item_typeObject = ItemTypes("Test_Data/test_")
 
     def test_get_item_types(self):
 
