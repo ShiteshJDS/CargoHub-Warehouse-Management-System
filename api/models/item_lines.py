6bf3eb4847a998fb70e3a7a32b62a6b1fd90d1af
C-G-Santos <1073607@hr.nl>
2024-12-10 23:32:18 +0100

    Added get method to ItemLines model

--- api/models/item_lines.py
+++ api/models/item_lines.py
@@ -1,50 +1,75 @@
-import json
-
+# import json
+import sqlite3
 from models.base import Base
 
-ITEM_LINES = []
+# ITEM_LINES = []
 
 
 class ItemLines(Base):
-    def __init__(self, root_path, is_debug=False):
-        self.data_path = root_path + "item_lines.json"
-        self.load(is_debug)
+    def __init__(self, db_path):
+        self.db_path = db_path
 
+    # Helper method to interact with the database.
+    def execute_query(self, query, params=None, fetch_one=False, fetch_all=False):
+        with sqlite3.connect(self.db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params or [])
+            conn.commit()
+            if fetch_one:
+                return cursor.fetchone()
+            if fetch_all:
+                return cursor.fetchall()
+
+    # Retrieve all item lines from the database.
     def get_item_lines(self):
-        return self.data
+        query = "SELECT * FROM item_lines"
+        return self.execute_query(query, fetch_all=True)
 
+    # Retrieve a single item line by ID.
     def get_item_line(self, item_line_id):
-        for x in self.data:
-            if x["id"] == item_line_id:
-                return x
-        return None
+        query = "SELECT * FROM item_lines WHERE id = ?"
+        return self.execute_query(query, params=(item_line_id,), fetch_one=True)
+    
+    
+    # def __init__(self, root_path, is_debug=False):
+    #     self.data_path = root_path + "item_lines.json"
+    #     self.load(is_debug)
 
-    def add_item_line(self, item_line):
-        item_line["created_at"] = self.get_timestamp()
-        item_line["updated_at"] = self.get_timestamp()
-        self.data.append(item_line)
+    # def get_item_lines(self):
+    #     return self.data
 
-    def update_item_line(self, item_line_id, item_line):
-        item_line["updated_at"] = self.get_timestamp()
-        for i in range(len(self.data)):
-            if self.data[i]["id"] == item_line_id:
-                self.data[i] = item_line
-                break
+    # def get_item_line(self, item_line_id):
+    #     for x in self.data:
+    #         if x["id"] == item_line_id:
+    #             return x
+    #     return None
 
-    def remove_item_line(self, item_line_id):
-        for x in self.data:
-            if x["id"] == item_line_id:
-                self.data.remove(x)
+    # def add_item_line(self, item_line):
+    #     item_line["created_at"] = self.get_timestamp()
+    #     item_line["updated_at"] = self.get_timestamp()
+    #     self.data.append(item_line)
 
-    def load(self, is_debug):
-        if is_debug:
-            self.data = ITEM_LINES
-        else:
-            f = open(self.data_path, "r")
-            self.data = json.load(f)
-            f.close()
+    # def update_item_line(self, item_line_id, item_line):
+    #     item_line["updated_at"] = self.get_timestamp()
+    #     for i in range(len(self.data)):
+    #         if self.data[i]["id"] == item_line_id:
+    #             self.data[i] = item_line
+    #             break
 
-    def save(self):
-        f = open(self.data_path, "w")
-        json.dump(self.data, f)
-        f.close()
+    # def remove_item_line(self, item_line_id):
+    #     for x in self.data:
+    #         if x["id"] == item_line_id:
+    #             self.data.remove(x)
+
+    # def load(self, is_debug):
+    #     if is_debug:
+    #         self.data = ITEM_LINES
+    #     else:
+    #         f = open(self.data_path, "r")
+    #         self.data = json.load(f)
+    #         f.close()
+
+    # def save(self):
+    #     f = open(self.data_path, "w")
+    #     json.dump(self.data, f)
+    #     f.close()
