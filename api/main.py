33e57edbb1b88ba7c58d7de16fe80104e0030e87
C-G-Santos <1073607@hr.nl>
2025-01-15 15:15:26 +0100

    Updated the main so it always makes the testdb

--- api/main.py
+++ api/main.py
@@ -435,19 +435,18 @@ def StartWebAPI():
 
     # Create test database if it doesn't exist
     test_db_name = 'api/Tests/Test_Data/Cargohub_Test.db'
-    if not os.path.exists(test_db_name):
-        cargohub_db.create_clients_table(test_db_name, 'data/clients.json')
-        cargohub_db.create_inventories_table(test_db_name, 'data/inventories.json')
-        cargohub_db.create_item_groups_table(test_db_name, 'data/item_groups.json')
-        cargohub_db.create_item_lines_table(test_db_name, 'data/item_lines.json')
-        cargohub_db.create_item_types_table(test_db_name, 'data/item_types.json')
-        cargohub_db.create_items_table(test_db_name, 'data/items.json')
-        cargohub_db.create_locations_table(test_db_name, 'data/locations.json')
-        cargohub_db.create_orders_table(test_db_name, 'data/orders.json')
-        cargohub_db.create_shipments_table(test_db_name, 'data/shipments.json')
-        cargohub_db.create_suppliers_table(test_db_name, 'data/suppliers.json')
-        cargohub_db.create_transfers_table(test_db_name, 'data/transfers.json')
-        cargohub_db.create_warehouses_table(test_db_name, 'data/warehouses.json')
+    cargohub_db.create_clients_table(test_db_name, 'data/clients.json')
+    cargohub_db.create_inventories_table(test_db_name, 'data/inventories.json')
+    cargohub_db.create_item_groups_table(test_db_name, 'data/item_groups.json')
+    cargohub_db.create_item_lines_table(test_db_name, 'data/item_lines.json')
+    cargohub_db.create_item_types_table(test_db_name, 'data/item_types.json')
+    cargohub_db.create_items_table(test_db_name, 'data/items.json')
+    cargohub_db.create_locations_table(test_db_name, 'data/locations.json')
+    cargohub_db.create_orders_table(test_db_name, 'data/orders.json')
+    cargohub_db.create_shipments_table(test_db_name, 'data/shipments.json')
+    cargohub_db.create_suppliers_table(test_db_name, 'data/suppliers.json')
+    cargohub_db.create_transfers_table(test_db_name, 'data/transfers.json')
+    cargohub_db.create_warehouses_table(test_db_name, 'data/warehouses.json')
     
     PORT = 3000
     with socketserver.TCPServer(("", PORT), ApiRequestHandler) as httpd:
